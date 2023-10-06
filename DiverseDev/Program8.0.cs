68afa52f8a639ee353911f30646508f4702f077b
Tiago Cruz <128756251+tiioliveira@users.noreply.github.com>
2023-10-06 19:24:39 -0300

    Add files via upload

---
+++ DiverseDev/Program8.0.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace DiverseDev
+{
+    class Desafios
+    {
+
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3;
+
+            Console.WriteLine();
+            Console.Write("Nota 1:");
+            nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Nota 2:");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Nota 3:");
+            nota3 = double.Parse(Console.ReadLine());
+
+
+        }
+
+        public static int MediaPrimaria(double nota1,double nota2,double nota3)
+        {
+
+            double media = nota1 + nota2 + nota3 / 3;         
+
+            if (media >= 7)
+            {
+                Console.WriteLine("O aluno foi aprovado com media:" + media);
+            }
+            else
+            {
+                Console.WriteLine("O aluno não foi aprovado com média " + media);
+                Console.WriteLine("Informe a nota do exame final:");
+                double notaEx = int.Parse(Console.ReadLine());
+                              
+                double mediaF = media + notaEx / 2;
+
+                if (mediaF >= 5)
+                {
+                    Console.WriteLine("O aluno foi aprovado no exame final com média final" + mediaF );
+                }
+                else
+                {
+                    Console.WriteLine("O aluno foi reprovado no exame final com média final " + mediaF);
+                }
+
+
+            }
+            return 0;
+        }
+        
+    }
+        
+}
---
+++ DiverseDev/Program9.0.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Threading.Channels;
+
+namespace DiverseDev
+{
+    class Desafios
+    {
+
+        static void Main(string[] args)
+        {
+            int f1, f2, f3, f4;
+            string n1, n2, n3, n4;
+            string nome;
+
+            n1 = "Fisica";
+            n2 = "Programação";
+            n3 = "Matematica";
+            n4 = "Filosofia";
+
+            f1 = 1;
+            f2 = 2;
+            f3 = 3;
+            f4 = 4;
+
+
+            Console.WriteLine("Por favor digite seu nome");
+            nome = Console.ReadLine();
+
+
+            Console.WriteLine("Escolha o curso que você quer ingressar\n");
+
+            Console.WriteLine(f1 + "- " + n1);
+            Console.WriteLine(f2 + "- " + n2);
+            Console.WriteLine(f3 + "- " + n3);
+            Console.WriteLine(f4 + "- " + n4);
+
+            int escolha = int.Parse(Console.ReadLine());
+
+
+            switch (escolha)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Parabens "+nome+" você está matriculado em Fisica, Bons estudos porque você ira precisar!!!");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Parabens "+nome+" você está matriculado em Programação, Que Deus guie seu caminho meu pobre usuario!");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Parabens "+nome+" você está matriculado em Matetica, Vai saber né, exite maluco pra tudo!!!");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Parabens "+nome+" você está matriculado em Filosofia, KKKKKKKKKKKKKKKKKKKK AI AI!!! ");
+                        break;
+                    }
+                default:
+
+                    Console.WriteLine("NUMERO INCORRETO");
+                    break;
+
+            }
+
+        }
+    }
+    
+}
