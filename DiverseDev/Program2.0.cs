64233653f6533e836a594f52981beb2a67b3dfc9
Tiago Cruz <128756251+tiioliveira@users.noreply.github.com>
2023-10-06 15:38:49 -0300

    Add files via upload

---
+++ DiverseDev/Program2.0.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Globalization;
+
+namespace DiverseDev
+{
+    class Desafios
+    {
+    public static void Main(string[] args)
+{
+
+    int anoA;
+    int nasC;
+
+    Console.WriteLine("Informe o ano que voce nasceu: ");
+    nasC = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Informe o seu ano atual: ");
+    anoA = int.Parse(Console.ReadLine());
+
+    int idade;
+
+    idade = SuaIdade(anoA, nasC);
+
+}
+
+    public static int SuaIdade(int anoA, int nasC)
+{
+
+    int idadeCR = (anoA - nasC);
+    Console.WriteLine("Sua idade é:" + idadeCR);
+    return idadeCR;
+
+}
+
+ }
+
+}
